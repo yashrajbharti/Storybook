4cbadd256688eedc2795a92026563d187a403dc8
yashrajbharticybtekk <y.bharti@cybtekk.com>
2024-11-30 23:06:51 +0530

    add: lang support to read aloud component

--- js/reader.mjs
+++ js/reader.mjs
@@ -3,12 +3,22 @@ import shoot from "./confetti.mjs";
 const template = document.getElementById("read-aloud-template").content;
 const form = document.getElementById("read-aloud-form");
 
+const langMap = new Map([
+  ["en", "en-US"],
+  ["es", "es-ES"],
+  ["ja", "ja-JP"],
+]);
+
 function read(event) {
   event.preventDefault();
-  const text = document.getElementById("textarea").value;
+  const textArea = document.getElementById("textarea");
+  const text = textArea.value;
   form.style.display = "none";
   const clone = document.importNode(template, true);
   clone.querySelector("p").textContent = text;
+  clone
+    .querySelector("read-aloud-component")
+    .setAttribute("lang", langMap.get(textArea.dataset.language));
   document.body.appendChild(clone);
   const readAloudComponent = document.querySelector("read-aloud-component");
   readAloudComponent.addEventListener("reading-complete", (event) => {
